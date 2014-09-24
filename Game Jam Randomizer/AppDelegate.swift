8fd1ea6ad9bcba459eacbf262a98f39a4f30b024
Saren Currie <scur778@aucklanduni.ac.nz>
2014-09-24 23:43:11 +1200

    Added licence comment

--- Game Jam Randomizer/AppDelegate.swift
+++ Game Jam Randomizer/AppDelegate.swift
@@ -1,10 +1,20 @@
-//
-//  AppDelegate.swift
-//  Game Jam Randomizer
-//
-//  Created by Saren Currie on 31/07/14.
-//  Copyright (c) 2014 Saren Currie. All rights reserved.
-//
+/*
+ * Game Jam Randomizer - Draws Random Themes and Things From a List.
+ * Copyright (C) 2014 Saren Currie
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import Cocoa
 
